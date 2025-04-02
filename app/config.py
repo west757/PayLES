68a0452d1ab59356fcf222efc37595b53cf77967
PayLES <mande185@asu.edu>
2025-04-01 20:18:28 -0600

    Added state and federal tax files

--- app/config.py
+++ app/config.py
@@ -16,6 +16,8 @@ class Config:
     PAY_DRILL_FILE = "pay_drill_2025.csv"
     BAH_WITH_DEPENDENTS_FILE = "bah_with_dependents_2025.csv"
     BAH_WITHOUT_DEPENDENTS_FILE = "bah_without_dependents_2025.csv"
+    FEDERAL_TAX_RATE_FILE = "federal_tax_rate_2024.csv"
+    STATE_TAX_RATE_FILE = "satte_tax_rate_2025.csv"
 
     MONTHS_LONG = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     MONTHS_SHORT = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
@@ -23,9 +25,9 @@ class Config:
                    'Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota',
                    'Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina',
                    'North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas',
-                   'Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+                   'Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming', 'District of Columbia']
     STATES_SHORT = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO',
-                    'MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
+                    'MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY', 'DC']
     RANKS_SHORT = ['E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9',
                    'W1', 'W2', 'W3', 'W4', 'W5', 'O1E', 'O2E', 'O3E',
                    'O1', 'O2', 'O3', 'O4', 'O5', 'O6', 'O7', 'O8', 'O9']
