91d67573fbcf85116d46b6e255586c34f4c2a0d2
west757 <westtech7@pm.me>
2025-08-16 12:03:51 -0600

    Updated home_form to use WTForms

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_session import Session
+from flask_wtf import CSRFProtect
 from app.config import Config
 
 flask_app = Flask(__name__)
 flask_app.config.from_object(Config)
 Session(flask_app)
+CSRFProtect(flask_app)
 
 import app.routes
 import app.errors
