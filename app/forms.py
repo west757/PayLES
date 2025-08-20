dc375727bdc28cfd1b7c08e8567686c722615304
west757 <westtech7@pm.me>
2025-08-19 23:29:55 -0600

    Starting updating expand_budget

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import Form, SelectField, DecimalField, IntegerField, StringField, SubmitField, FileField, FieldList, FormField
-from wtforms.validators import NumberRange, Length, Regexp
+from wtforms import SubmitField, FileField
 
 class HomeForm(FlaskForm):
     home_input = FileField('Drop LES here')
     submit_les = SubmitField('Submit LES')
     submit_example = SubmitField('Example LES')
-
-
-class SettingsForm(FlaskForm):
-    months_display = SelectField('Months Displayed:', choices=[(str(x), str(x)) for x in range(2, 13)])
