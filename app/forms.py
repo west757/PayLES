3f364c14ba4f25bd6d3d51b313358bd88a6eb608
west757 <westtech7@pm.me>
2025-08-31 17:39:17 -0600

    Updating home page and forms

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, FileField
+from wtforms import SubmitField, FileField, SelectField
 
-class HomeForm(FlaskForm):
-    home_input = FileField('Drop LES here')
-    submit_les = SubmitField('Submit LES')
-    submit_example = SubmitField('Example LES')
+class FormSingleLES(FlaskForm):
+    les_input = FileField('Drop LES here')
+    submit_single_les = SubmitField('Submit LES')
+
+class FormJointLES(FlaskForm):
+    joint_les_1 = FileField('Drop Member 1 LES')
+    joint_les_2 = FileField('Drop Member 2 LES')
+    submit_joint_les = SubmitField('Submit Both LES')
+
+class FormWithoutLES(FlaskForm):
+    grade = SelectField('Grade', choices=[('E1', 'E1'), ('E2', 'E2')])
+    submit_without_les = SubmitField('Submit Without LES')
+
+class FormExampleLES(FlaskForm):
+    submit_example_les = SubmitField('Submit Example LES')
